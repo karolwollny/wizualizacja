4510b32f1784db8ce49481235b9a0ee1b88747f4
Karol Wollny <karol.wollny@interia.pl>
2020-11-30 21:29:12 +0100

    added wordclouds for negative and positive persons

--- src/main.py
+++ src/main.py
@@ -2,11 +2,16 @@ import pandas as pd
 from flair.data import Sentence
 from flair.models import SequenceTagger
 from flair.models import TextClassifier
+from wordcloud import WordCloud
+import matplotlib.pyplot as plt
+import numpy as np
+from PIL import Image
 
 ###### FASTTEXT IMPORTS
 import fasttext
 
 DATASET_FILE = "../data/Shakespeare_data.csv"
+MASK_FILE = "../data/william-shakespeare-black-silhouette.jpg"
 
 
 def load_dataset_from(csv_file: str) -> pd.DataFrame:
@@ -35,13 +40,54 @@ if __name__ == '__main__':
     # print(df.head(10))
 
     # Extract all players lines and cast it to list
-    text = df["PlayerLine"].tolist()
-    # Cast test lines into sentences so it can be leverage by flair
-    sentences_list = [Sentence(line) for line in text]
+
+    tmp = {}
+    for dp in df.values:
+        l = dp.tolist()
+        tmp[str(l[-2])] = " ".join([tmp[str(l[-2])], l[-1]]) if str(l[-2]) in tmp.keys() else l[-1]
 
     classifier = TextClassifier.load('sentiment')
-    classifier.predict(sentences_list)
-    print(sentences_list[10])
+
+    negative = {}
+    positive = {}
+
+    for player in tmp.keys():
+        tmp[player] = Sentence(tmp[player])
+        classifier.predict(tmp[player])
+        if tmp[player].labels[0].value == "NEGATIVE":
+            negative[player] = tmp[player].labels[0].score
+        elif tmp[player].labels[0].value == "POSITIVE":
+            positive[player] = tmp[player].labels[0].score
+
+    wordcloud_negative = WordCloud(background_color='black',
+                          width=500,
+                          height=250,
+                          relative_scaling=1,
+                          ).generate_from_frequencies(negative)
+
+    plt.imshow(wordcloud_negative, interpolation='bilinear')
+    plt.title("NEGATIVE")
+    plt.axis("off")
+    plt.show()
+
+    wordcloud_positive = WordCloud(background_color='black',
+                          width=500,
+                          height=250,
+                          relative_scaling=1,
+                          ).generate_from_frequencies(positive)
+
+    plt.imshow(wordcloud_positive, interpolation='bilinear')
+    plt.title("POSITIVE")
+    plt.axis("off")
+    plt.show()
+
+    #text = df["PlayerLine"].tolist()
+    # Cast test lines into sentences so it can be leverage by flair
+    # sentences_list = [Sentence(line) for line in text[:100]]
+    #
+    # classifier = TextClassifier.load('sentiment')
+    # classifier.predict(sentences_list)
+    # print(sentences_list[10])
 
     # model = fasttext.train_unsupervised(DATASET_FILE, model='skipgram')
     # print(len(model.words))
