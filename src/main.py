fe1afe19ab4f633aea6717d6e2df705dafdc0b12
Karol Wollny <karol.wollny@interia.pl>
2020-12-15 20:38:23 +0100

    Added sentiment for each player line; added parsing into json format

--- src/main.py
+++ src/main.py
@@ -6,11 +6,12 @@ from wordcloud import WordCloud
 import matplotlib.pyplot as plt
 import numpy as np
 from PIL import Image
+import json
 
 ###### FASTTEXT IMPORTS
 import fasttext
 
-DATASET_FILE = "../data/Shakespeare_data.csv"
+DATASET_FILE = "../data/new/Hamlet.csv"
 MASK_FILE = "../data/william-shakespeare-black-silhouette.jpg"
 
 
@@ -39,26 +40,50 @@ if __name__ == '__main__':
     # To verify if data loaded correctly:
     # print(df.head(10))
 
-    # Extract all players lines and cast it to list
+    classifier = TextClassifier.load('sentiment')
 
-    tmp = {}
+    tmp_negatives = {}
+    tmp_positives = {}
+
+    print(f'Number of players: {len(df["Player"].unique())}')
+
+    for player_name in df["Player"].unique():
+        tmp_negatives[player_name] = list()
+        tmp_positives[player_name] = list()
+
     for dp in df.values:
         l = dp.tolist()
-        tmp[str(l[-2])] = " ".join([tmp[str(l[-2])], l[-1]]) if str(l[-2]) in tmp.keys() else l[-1]
-
-    classifier = TextClassifier.load('sentiment')
+        sentence = Sentence(l[-1])
+        classifier.predict(sentence)
+        if sentence.labels[0].value == "NEGATIVE":
+            tmp_negatives[l[-2]].append(sentence.labels[0].score)
+        elif sentence.labels[0].value == "POSITIVE":
+            tmp_positives[l[-2]].append(sentence.labels[0].score)
 
     negative = {}
     positive = {}
 
-    for player in tmp.keys():
-        tmp[player] = Sentence(tmp[player])
-        classifier.predict(tmp[player])
-        if tmp[player].labels[0].value == "NEGATIVE":
-            negative[player] = tmp[player].labels[0].score
-        elif tmp[player].labels[0].value == "POSITIVE":
-            positive[player] = tmp[player].labels[0].score
+    for player in tmp_negatives.keys():
+        if len(tmp_negatives[player]) > 10:
+            negative[player] = sum(tmp_negatives[player]) / len(tmp_negatives[player])
 
+    for player in tmp_positives.keys():
+        if len(tmp_positives[player]) > 10:
+            positive[player] = sum(tmp_positives[player]) / len(tmp_positives[player])
+
+    print(negative)
+    print(positive)
+
+    # for player in tmp.keys():
+    #     tmp[player] = Sentence(tmp[player])
+    #     classifier.predict(tmp[player])
+    #     if tmp[player].labels[0].value == "NEGATIVE":
+    #         negative[player] = tmp[player].labels[0].score
+    #     elif tmp[player].labels[0].value == "POSITIVE":
+    #         positive[player] = tmp[player].labels[0].score
+    #
+    f = plt.figure()
+    f.add_subplot(2, 1, 1)
     wordcloud_negative = WordCloud(background_color='black',
                           width=500,
                           height=250,
@@ -68,20 +93,25 @@ if __name__ == '__main__':
     plt.imshow(wordcloud_negative, interpolation='bilinear')
     plt.title("NEGATIVE")
     plt.axis("off")
-    plt.show()
+    #plt.show()
 
     wordcloud_positive = WordCloud(background_color='black',
                           width=500,
                           height=250,
                           relative_scaling=1,
                           ).generate_from_frequencies(positive)
-
+    f.add_subplot(2, 1, 2)
     plt.imshow(wordcloud_positive, interpolation='bilinear')
     plt.title("POSITIVE")
     plt.axis("off")
     plt.show()
 
-    #text = df["PlayerLine"].tolist()
+    positive_json = json.dumps(positive, indent=4)
+    negative_json = json.dumps(negative, indent=4)
+
+
+
+    # text = df["PlayerLine"].tolist()
     # Cast test lines into sentences so it can be leverage by flair
     # sentences_list = [Sentence(line) for line in text[:100]]
     #
